0fb6e6e22b2dd970e5a3c2b7fbe25025a83a69fd
Marcelo <marceloparducci@gmail.com>
2026-04-30 20:19:07 -0300

    Add firebase.js to repo for Netlify build

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult, signOut } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJoYH_adR6ExriM6-Vq4o-Aathh1TxRTU",
+  authDomain: "on-the-rocks-2ef88.firebaseapp.com",
+  projectId: "on-the-rocks-2ef88",
+  storageBucket: "on-the-rocks-2ef88.firebasestorage.app",
+  messagingSenderId: "262719751691",
+  appId: "1:262719751691:web:c8ae9bba551880d1dae90e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
+
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+export const signInWithGoogle = () =>
+  isMobile ? signInWithRedirect(auth, googleProvider) : signInWithPopup(auth, googleProvider);
+
+export { getRedirectResult };
+export const signOutUser = () => signOut(auth);
